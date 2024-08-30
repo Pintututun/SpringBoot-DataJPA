6ace0b5f592d5b8e2b1880f7999aa88b1439c301
Pintututun <priyabratasatapathy00@gmail.com>
2024-08-30 11:44:17 +0530

    1st project

---
+++ SpringBootCrudRepositoryUsingMySQLDatabase/src/main/java/in/ashokit/entity/Employee.java
@@ -0,0 +1,62 @@
+package in.ashokit.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+
+
+
+@Entity
+public class Employee {
+
+	@Id
+	private Integer eid; // findById
+	private String ename; // findByEname
+	@Transient
+	private Double esalary; // findByEsalary
+	
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+		public Employee(Integer eid, String ename, Double esalary) {
+			super();
+			this.eid = eid;
+			this.ename = ename;
+			this.esalary = esalary;
+		}
+
+	public Integer getEid() {
+		return eid;
+	}
+
+	public void setEid(Integer eid) {
+		this.eid = eid;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public Double getEsalary() {
+		return esalary;
+	}
+
+	public void setEsalary(Double esalary) {
+		this.esalary = esalary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + ", esalary=" + esalary + "]";
+	}
+	
+    
+
+
+}
---
+++ SpringBootCrudRepositoryUsingMySQLDatabase/src/main/java/in/ashokit/service/EmployeeService.java
@@ -0,0 +1,99 @@
+package in.ashokit.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import in.ashokit.entity.Employee;
+import in.ashokit.repo.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+	@Autowired
+	private EmployeeRepository repo;
+	
+	public void createSingleRecord(Employee e)
+	{
+		Employee emp=repo.save(e);
+		System.out.println(emp);
+	}
+	
+	public void createMultipleRecords(List<Employee> list)
+	{
+		 Iterable<Employee> saveAll = repo.saveAll(list);
+		 Iterator<Employee> itr=saveAll.iterator();
+		 while(itr.hasNext())
+		 {
+			 System.out.println(itr.next());
+		 }
+	}
+	public void updateById(Employee e)
+	{
+		Employee emp=repo.save(e);
+		System.out.println(emp);
+	}
+	public void countRecords()
+	{
+		Long num=repo.count();
+		System.out.println("Number of records "+num);
+	}
+	
+	public void deleteSingleRecord(Employee e)
+	{
+		
+		repo.delete(e);
+		System.out.println("Deleted successfully");
+		
+	}
+	public void deleteMultipleRecords(List<Employee> list)
+	{
+		repo.deleteAll(list);
+		System.out.println("Deleted Successfully");
+	}
+	public void deleteAllRecords()
+	{
+		repo.deleteAll();
+		System.out.println("Deleted Successfully");
+	}
+	public void deleteById(Integer id)
+	{
+		repo.deleteById(id);
+		System.out.println("Deleted Successfully");
+	}
+	public void deleteByIds(List<Integer> list)
+	{
+		repo.deleteAllById(list);
+		System.out.println("Deleted Successfully");
+	}
+	public void showAllRecords()
+	{
+		Iterable<Employee> allRecords = repo.findAll();
+		Iterator<Employee> itr=allRecords.iterator();
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+		
+	}
+	public void showRecordById(Integer id)
+	{
+		Optional<Employee> emp = repo.findById(id);
+		if (emp.isPresent()) {
+			Employee employee = emp.get();
+			System.out.println(employee);
+		} else {
+			System.out.println("Record Not Found");
+		}
+	}
+	public void showRecordsByIds(List<Integer> list)
+	{
+		Iterable<Employee> emps = repo.findAllById(list);
+		emps.forEach(e->System.out.println(e));
+	}
+}
